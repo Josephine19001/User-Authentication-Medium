f27d68ee2f98203f3a0e07fa5192795471fcced7
Josephine19001 <gyameraj@gmail.com>
2020-08-18 09:03:33 +0300

    client folder error fixed

---
+++ keys.js
@@ -0,0 +1,7 @@
+const mongoUri = {
+  MONGO_URI:
+    "mongodb+srv://josephine19001:22051999Jo@cluster0.njqvb.mongodb.net/authentication-tutorial?retryWrites=true&w=majority",
+  JWT_SECRET: "MYSECRET",
+};
+
+export default mongoUri;
